ccbafdb30cfa3c7acd068f9a70518abb291d4883
Nikitavish123 <nikita.vishwakarma@softobotics.com>
2024-01-11 16:11:49 +0530

    side title changed

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('page_title', 'Softobotics')
+@section('page_title', 'My Dashboard')
 
 @section('content')
     <h2>WELCOME {{ Auth::user()->name }}. This is your DASHBOARD</h2>

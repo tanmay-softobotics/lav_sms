ce79ffc553c7df7d8161ffa4b44da8137f876a9a
Tanmay Vij <83805724+tanmay-softobotics@users.noreply.github.com>
2024-11-10 00:57:09 +0530

    Update Qs.php

--- app/Helpers/Qs.php
+++ app/Helpers/Qs.php
@@ -31,7 +31,7 @@ class Qs
 
     public static function getDefaultUserImage()
     {
-        return ('http://3.110.204.208/global_assets/images/user.png');
+        return ('/global_assets/images/user.png');
     }
 
     public static function getPanelOptions()
